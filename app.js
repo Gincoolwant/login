60049a38ff4fdc528d83c132bd166f95a9099e4f
Gincoolwant <soulbox790326@gmail.com>
2022-10-23 00:46:57 +0800

    feat: login index page and btn

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname:'hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
