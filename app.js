56c084b6e3d77a9f11398d2649c5032bac37e1fb
Gincoolwant <soulbox790326@gmail.com>
2022-10-24 02:38:03 +0800

    feat: cookie-based authentication

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const mongoose = require('./config/mongoose.js')
 const routes = require('./routes/index.js')
 
@@ -11,6 +12,7 @@ app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser('ckfirstcookie'))
 
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/users')
 
+
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { name: req.signedCookies.user })
 })
 
 router.post('/login', (req, res) => {
@@ -11,10 +12,17 @@ router.post('/login', (req, res) => {
   User.findOne({ $and: [{ email }, { password }] })
     .lean()
     .then(user => {
-      if (!user) return res.render('index', { user: !user, email, password })
+      if (!user) return res.render('index', { name: req.signedCookies.user, user: !user, email, password })
+
+      res.cookie('user', user.firstName, { signed: true });
       return res.render('welcome', { user })
     })
     .catch(error => console.error(error))
+})
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('user')
+  return res.redirect('/')
 })
 
 module.exports = router
